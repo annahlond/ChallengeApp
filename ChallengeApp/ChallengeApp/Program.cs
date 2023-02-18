0ee965c5ecbd47e151b6f1ec2b98dca207b9c21f
annahlond <anna.hlond@gmail.com>
2023-02-18 20:21:44 +0100

    Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,60 +1,58 @@
-﻿// liczby całkowite
-var myAge = 120;
-int newAge = myAge + 4;
-int newAge1 = myAge + 5;
-int newAge2 = newAge + 4;
-Console.WriteLine(newAge1);
-Console.WriteLine(newAge2);
-int myVar = int.MinValue;
-int myVar2 = int.MaxValue;
-Console.WriteLine(myVar);
-Console.WriteLine(myVar2);
-uint myVar3 = uint.MaxValue;
-uint myVar4 = uint.MinValue;
-Console.WriteLine(myVar3);
-Console.WriteLine(myVar4);
-long myVar5 = long.MinValue;
-ulong myVar6 = ulong.MaxValue;
-Console.WriteLine(myVar5);
-Console.WriteLine(myVar6);
-//liczby zmiennoprzecinkowe
-float myNumber1 = float.MinValue;
-double myNumber2 = double.MaxValue;
-Console.WriteLine(myNumber1);
-Console.WriteLine(myNumber2);
-decimal myNumber3 = decimal.MinValue;
-Console.WriteLine(myNumber3);
+﻿string name = "Ewa";
+int age = 33;
+char plec = 'K';
 
-// tekst
-string name = "Anna ";
-string surname = "Hlond ";
-string result = name + surname + myAge;
-Console.WriteLine(result);
-char myVar7 = 'c';
-Console.WriteLine(myVar7);
-
-
-char var10 = 'a';
-Console.WriteLine(var10);
-
-var result2 = name.ToArray();
-Console.WriteLine(result2);
-
-// zmienna logiczna
-bool isActive = true;
-isActive = false;
-var isValid = 5 > 6;
-Console.WriteLine(isValid);
-
-var number1 = 5;
-var number2 = 10;
-if (number1 < number2)
-{   
-    Console.WriteLine("Jestem tutaj w linii 5");
- 
+if (plec == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
 }
 else
 {
-    Console.WriteLine("Jestem tutaj w linii 58");
-
+    Console.WriteLine("Nie jesteś kobietą poniżej 30 lat");
 }
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else
+{
+    Console.WriteLine("Nie jesteś 33 letnią Ewą");
+}
+
+if (plec == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Nie jesteś niepełnoletnim mężczyzną");
+}
+
+Console.WriteLine("Poniżej drugie rozwiązanie, bo nie byłam pewna czego oczekujesz:");
+
+if (plec == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else
+    {
+        if (plec == 'M' && age < 18)
+        {
+            Console.WriteLine("Niepełnoletni mężczyzna");
+        }
+        else
+        {
+            Console.WriteLine("Nie jesteś ani niepełnoletnim mężczyzną");
+            Console.WriteLine("ani 33-letnią Ewą ani kobietą poniżej 30");
+        }
+
+    }
+}
+
