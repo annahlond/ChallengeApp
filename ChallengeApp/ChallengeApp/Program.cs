ecab3bd9a98d1794d15d46c62ad50b10ed125dda
annahlond <anna.hlond@gmail.com>
2023-02-19 23:29:42 +0100

    Dzień  5 zadanie dodatkowe 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,11 +1,14 @@
-﻿List<string> daysOfWeek = new List<string>();
-daysOfWeek.Add("poniedziałek");
-daysOfWeek.Add("wtorek");
-daysOfWeek.Add("środa");
-daysOfWeek.Add("czwarek");
-daysOfWeek.Add("piątek");
-daysOfWeek.Add("sobota");
-daysOfWeek.Add("niedziela");
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
 
-int numberOfDayOfWeek = 3;
-Console.WriteLine(numberOfDayOfWeek + " " + daysOfWeek[numberOfDayOfWeek - 1]);
+char temperatureScale = 'F';
+float temperature = 36.7f;
+if (temperatureScale == 'C')
+{
+    float convertedTemperature = temperature * 1.8f +32;
+    Console.WriteLine(temperature + "C = " + string.Format("{0:N1}", convertedTemperature) + 'F');
+}
+else
+{
+    float convertedTemperature = (temperature - 32) / 1.8f;
+    Console.WriteLine(temperature + "F = " + string.Format("{0:N1}", convertedTemperature) + 'C');
+}
