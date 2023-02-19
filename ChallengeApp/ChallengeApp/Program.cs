c0b640e6c6a6726dbd4370e149322492ec195ee9
annahlond <anna.hlond@gmail.com>
2023-02-19 22:21:06 +0100

    DZień 5 zadanie dodatkowe 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,6 +1,11 @@
-﻿char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-for (int i = 0;i < alpha.Length; i++)
-{
-    Console.WriteLine(alpha[i]);
-}
+﻿List<string> daysOfWeek = new List<string>();
+daysOfWeek.Add("poniedziałek");
+daysOfWeek.Add("wtorek");
+daysOfWeek.Add("środa");
+daysOfWeek.Add("czwarek");
+daysOfWeek.Add("piątek");
+daysOfWeek.Add("sobota");
+daysOfWeek.Add("niedziela");
 
+int numberOfDayOfWeek = 3;
+Console.WriteLine(daysOfWeek[numberOfDayOfWeek - 1]);
