e7e3a6bd97a52f23ed001ac7e71ac68b6802ccff
annahlond <anna.hlond@gmail.com>
2023-02-20 19:43:42 +0100

    DZień 5 zadanie dodatkowe 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,14 +1,10 @@
 ﻿using static System.Runtime.InteropServices.JavaScript.JSType;
 
-char temperatureScale = 'F';
-float temperature = 36.7f;
-if (temperatureScale == 'C')
-{
-    float convertedTemperature = temperature * 1.8f +32;
-    Console.WriteLine(temperature + "C = " + string.Format("{0:N1}", convertedTemperature) + 'F');
-}
-else
-{
-    float convertedTemperature = (temperature - 32) / 1.8f;
-    Console.WriteLine(temperature + "F = " + string.Format("{0:N1}", convertedTemperature) + 'C');
-}
+int numberOfDays = 7856;
+Console.WriteLine("liczba dni = " + numberOfDays);
+int numberOfYears = numberOfDays / 365;
+Console.WriteLine("liczba lat = " + numberOfYears);
+int numberOfWeeks = (numberOfDays - numberOfYears * 365) / 7;
+Console.WriteLine("liczba tygodni = " + numberOfWeeks);
+int remainedNumberOfDays = numberOfDays - numberOfYears * 365 - numberOfWeeks * 7;
+Console.WriteLine("pozostała liczba dni = " + remainedNumberOfDays);
