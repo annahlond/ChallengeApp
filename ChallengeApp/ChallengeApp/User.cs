e4154964d7ce3ccd29c70d013ebc402611d71156
annahlond <anna.hlond@gmail.com>
2023-03-24 17:36:50 +0100

    Dzień 6

---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,39 @@
+﻿namespace ChallengeApp
+{
+    public class User
+    {
+       
+            public static string GameName = "Diablo";
+
+            private string login;
+            private string password;
+
+            private List<int> score = new List<int>();
+
+            public User(string login, string password)
+            {
+                this.login = login;
+                this.password = password;
+
+
+            }
+
+            public string Login { get; private set; }
+            public string Password { get; private set; }
+
+            public int Result
+            {
+                get
+                {
+                    return this.score.Sum();
+                }
+            }
+            public void AddScore(int number)
+            {
+                this.score.Add(number);
+            }
+        
+
+
+    }
+}
