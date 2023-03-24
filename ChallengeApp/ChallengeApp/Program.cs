daa87ae07f9ebad3abea1bf23a16cdf4071c5da5
annahlond <anna.hlond@gmail.com>
2023-03-24 17:34:58 +0100

    dzień 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,15 +1,19 @@
-﻿float number = -35.47f;
-Console.WriteLine(number);
-if (number < 0)
-{
-    Console.WriteLine("dana liczba jest ujemna");
-}
-else if (number > 0)
-{
-    Console.WriteLine("dana liczba jest dodatnia");
-}
-else
-{
-    Console.WriteLine("dana liczba to 0");
-}
+﻿using ChallangeApp;
+
+User user1= new User("Adam", "haslo1");
+User user2= new User("Zuza", "haslo2");
+User user3= new User("Anka", "haslo3");
+User user4= new User("Monika", "haslo4");
+
+
+var name = user1.Login;
+
+user1.AddScore(5);
+user1.AddScore(2);
+var result = user1.Result;
+Console.WriteLine(result);
+var name1 = User.GameName;
+var pi = Math.PI;
+
+
 
