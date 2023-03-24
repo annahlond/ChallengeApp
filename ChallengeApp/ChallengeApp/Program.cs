cc7eb711e5dbca886eddb8d8d9012774f2413922
annahlond <anna.hlond@gmail.com>
2023-03-24 19:02:03 +0100

    Dzień 6 zadanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,19 +1,52 @@
-﻿using ChallangeApp;
-
-User user1= new User("Adam", "haslo1");
-User user2= new User("Zuza", "haslo2");
-User user3= new User("Anka", "haslo3");
-User user4= new User("Monika", "haslo4");
+﻿
+using ChallengeApp;
 
 
-var name = user1.Login;
+Employee employee1 = new Employee("Adam", "Michalski", 34);
+Employee employee2 = new Employee("Anna", "Hlond", 53);
+Employee employee3 = new Employee("Michał", "Polański", 41);
 
-user1.AddScore(5);
-user1.AddScore(2);
-var result = user1.Result;
-Console.WriteLine(result);
-var name1 = User.GameName;
-var pi = Math.PI;
+
+
+employee1.AddScore(1); 
+employee1.AddScore(3);
+employee1.AddScore(6);
+employee1.AddScore(9);
+employee1.AddScore(10);
+employee2.AddScore(10);
+employee2.AddScore(1);
+employee2.AddScore(8);
+employee2.AddScore(4);
+employee2.AddScore(1);
+employee3.AddScore(2);
+employee3.AddScore(8);
+employee3.AddScore(7);
+employee3.AddScore(2);
+employee3.AddScore(1);
+
+List<Employee> employees = new List<Employee>()
+{
+    employee1, employee2, employee3
+};
+
+
+int maxResult = 0;
+Employee employeeWithMaxResult= null;
+
+foreach(var employee in employees)
+{
+    
+    if (employee.Result > maxResult)
+    {
+        employeeWithMaxResult= employee;
+        maxResult = employee.Result;
+    }
+}
+
+Console.WriteLine("Najwyższy wynik osiągnął: ");
+Console.WriteLine(employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + " " + employeeWithMaxResult.Age);
+Console.WriteLine("z wynikiem: " + maxResult);
+
 
 
 
