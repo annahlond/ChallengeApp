d5251b8ef2aa159b8abc20f334a5960891dca34a
annahlond <anna.hlond@gmail.com>
2023-03-28 20:19:22 +0200

    dzień 9

--- ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -4,7 +4,7 @@ namespace ChallengeApp.Tests
     {
        
         [Test]
-        public void SumOfGradesShouldGetCorrectResult()
+        public void GetStatisticsShouldGiveCorrectMax()
         {
             //arrange
             var employee = new Employee("Adam", "Niedzielski");
@@ -12,15 +12,51 @@ namespace ChallengeApp.Tests
             employee.AddGrade(6);
             employee.AddGrade(10);
             employee.AddGrade(9);
-            employee.AddGrade(-25);
+            employee.AddGrade(5);
 
             //act
-            
+            var statistics = employee.GetStatistics();
 
             //assert
-           // Assert.AreEqual(0, result);
+            Assert.AreEqual(10, statistics.Max);
             
         }
-       
+        [Test]
+        public void GetStatiscticsShouldGiveCorrectMin()
+        {
+            //arrange
+            var employee = new Employee("Adam", "Niedzielski");
+            employee.AddGrade(0);
+            employee.AddGrade(6);
+            employee.AddGrade(10);
+            employee.AddGrade(9);
+            employee.AddGrade(5);
+
+            //act
+            var statistics = employee.GetStatistics();
+
+            //assert
+            Assert.AreEqual(0, statistics.Min);
+
+        }
+        [Test]
+        public void GetStatisticsShouldGiveCorrectAverage()
+        {
+            //arrange
+            var employee = new Employee("Adam", "Niedzielski");
+            employee.AddGrade(0);
+            employee.AddGrade(6);
+            employee.AddGrade(10);
+            employee.AddGrade(9);
+            employee.AddGrade(5);
+
+            //act
+            var statistics = employee.GetStatistics();
+
+            //assert
+            Assert.AreEqual(6, statistics.Average);
+
+        }
+
     }
 }
